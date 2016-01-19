c7a1a5d266ae098212a02a331e3275084806c069
Ivan Nečas <inecas@redhat.com>
2016-01-19 17:52:45 +0100

    Fixes #13278 - support for foreign input set

--- lib/hammer_cli_foreman_remote_execution.rb
+++ lib/hammer_cli_foreman_remote_execution.rb
@@ -7,6 +7,7 @@ module HammerCLIForemanRemoteExecution
   require 'hammer_cli_foreman_remote_execution/job_invocation'
   require 'hammer_cli_foreman_remote_execution/job_template'
   require 'hammer_cli_foreman_remote_execution/template_input'
+  require 'hammer_cli_foreman_remote_execution/foreign_input_set'
 
   def self.exception_handler_class
     HammerCLIForeman::ExceptionHandler
