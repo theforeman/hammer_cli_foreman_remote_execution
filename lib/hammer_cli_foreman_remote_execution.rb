a0d5ee69d045edc7a2ddc479639dd721a59109c1
Ivan Nečas <inecas@redhat.com>
2016-02-12 18:41:19 +0100

    Fixes #13691 - hammer CLI for developer API

--- lib/hammer_cli_foreman_remote_execution.rb
+++ lib/hammer_cli_foreman_remote_execution.rb
@@ -8,6 +8,7 @@ module HammerCLIForemanRemoteExecution
   require 'hammer_cli_foreman_remote_execution/job_template'
   require 'hammer_cli_foreman_remote_execution/template_input'
   require 'hammer_cli_foreman_remote_execution/foreign_input_set'
+  require 'hammer_cli_foreman_remote_execution/remote_execution_feature'
 
   def self.exception_handler_class
     HammerCLIForeman::ExceptionHandler
