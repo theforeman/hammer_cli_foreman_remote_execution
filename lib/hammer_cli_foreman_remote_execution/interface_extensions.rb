ef399679a0aa7b9a7ae5b9135079af47bf20b8dc
Maria <mariaagaphonchev@gmail.com>
2020-09-10 16:52:55 +0300

    Refs #20689 - add execution flag to interface (#35)

---
+++ lib/hammer_cli_foreman_remote_execution/interface_extensions.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'hammer_cli_foreman/interface'
+
+module HammerCLIForemanRemoteExecution
+  class InterfaceExtensionsInfo < ::HammerCLI::CommandExtensions
+    output do |definition|
+      definition.append do
+        field :execution, _('Execution'), Fields::Boolean
+      end
+    end
+  end
+
+  module InterfaceExtensionsList
+    def format_type(nic)
+      type = super(nic)
+      if nic['execution']
+        if nic['primary'] || nic['provision']
+          type[-1] = ', '+_('execution') +')'
+        else
+          type += ' ('+_('execution') +')'
+        end
+      end
+      type
+    end
+  end
+
+  ::HammerCLIForeman::Interface.singleton_class.prepend InterfaceExtensionsList
+  ::HammerCLIForeman::Interface::InfoCommand.extend_with(InterfaceExtensionsInfo.new)
+end
